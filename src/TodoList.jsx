be5c7aec3cbbeea387166f493933f617b6255b86
glyzaaa <apusglyzamae@gmail.com>
2025-04-21 11:49:21 +0800

    okay

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 
-const API_URL = "https://pit3-axj5.onrender.com/api/tasks/";
+const API_URL = "https://todo-fastapi-sjxd.onrender.com/todos/";
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([]);
